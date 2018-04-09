a461fab1439ab8f279ffcd3a7ce62270ba89a3be
hzchendawei <hzchendawei@corp.netease.com>
2018-04-09 20:31:01 +0800

    misc 实现根据bean生成建表语句、mapper.xml

--- akka-test/src/main/resources/temp/Model.java
+++ akka-test/src/main/resources/temp/Model.java
@@ -1,13 +1,15 @@
 package ${packageName};
 import lombok.Data;
 import lombok.ToString;
+import java.io.Serializable;
 
 @Data
 @ToString
-public interface ${beanName} {
+public class ${beanName}  implements Serializable{
     <#list propertiesList as p>
         <#if p.column== "id">private Long id;
         <#elseif p.jdbcType=="BIGINT">private Long ${p.property};
+        <#elseif p.jdbcType=="INTEGER">private Integer ${p.property};
         <#elseif p.jdbcType=="TIMESTAMP">private Date ${p.property};
         <#elseif p.jdbcType=="VARCHAR">private String ${p.property};
         <#else>  private Object ${p.property};
